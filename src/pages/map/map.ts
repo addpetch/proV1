700f91e61c0f2d95d2431655d8de47a60387c698
Kanokpop <kanokpop23@gmail.com>
2019-02-13 16:09:39 +0700

    Change the way to Compare Station Closer Station(All)

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -71,15 +71,26 @@ export class MapPage {
         
         ionViewDidEnter(){
           this.getPosition();
+          
           // this.getDataFromFirebase();
+          
         } 
+
+        calculateDistance(lat1:number,lat2:number,long1:number,long2:number){
+          let p = 0.017453292519943295;    // Math.PI / 180
+          let c = Math.cos;
+          let a = 0.5 - c((lat1-lat2) * p) / 2 + c(lat2 * p) *c((lat1) * p) * (1 - c(((long1- long2) * p))) / 2;
+          let dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
+          console.log(dis);
+          return dis;
+        }
+
         getDataFromFirebase(){return new Promise((resolve, reject) => {
           this.db.list('/Station/').valueChanges()
           .subscribe(
             data => {
               this.Pop = data;
               resolve(this.Pop)  
-
             },
             );
           });
@@ -109,6 +120,7 @@ export class MapPage {
                   console.log(error);
                 })
               }
+
               // getData(){
               //   this.getDataFromFirebase().then(data =>{
               //     //  item = data as any;
@@ -157,200 +169,232 @@ export class MapPage {
                     //   this.stLng = this.Pop[2];
                     // this.getData(); 
 
-        let bVicLatLng = {lat: this.Pop[0].lat, lng: this.Pop[0].lng};
-        let bPhaLatLng = {lat: this.Pop[1].lat, lng: this.Pop[1].lng};
-        let bRatLatLng = {lat: this.Pop[2].lat, lng: this.Pop[2].lng};
-        let bSiaLatLng = {lat: this.Pop[3].lat, lng: this.Pop[3].lng};
-        let bChiLatLng = {lat: this.Pop[4].lat, lng: this.Pop[4].lng};
-        let bPhlLatLng = {lat: this.Pop[5].lat, lng: this.Pop[5].lng};
-        let bNanLatLng = {lat: this.Pop[6].lat, lng: this.Pop[6].lng};
-        let bAsoLatLng = {lat: this.Pop[7].lat, lng: this.Pop[7].lng};
-        let mRamLatLng = {lat: this.Pop[8].lat, lng: this.Pop[8].lng};
-        let mPheLatLng = {lat: this.Pop[9].lat, lng: this.Pop[9].lng};
-        let mSukLatLng = {lat: this.Pop[10].lat, lng: this.Pop[10].lng};
-        let aPhaLatLng = {lat: this.Pop[11].lat, lng: this.Pop[11].lng};
-        let aRatLatLng = {lat: this.Pop[12].lat, lng: this.Pop[12].lng};
-        let aMukLatLng = {lat: this.Pop[13].lat, lng: this.Pop[13].lng};
+              let bVicLatLng = {lat: this.Pop[0].lat, lng: this.Pop[0].lng};
+              let bPhaLatLng = {lat: this.Pop[1].lat, lng: this.Pop[1].lng};
+              let bRatLatLng = {lat: this.Pop[2].lat, lng: this.Pop[2].lng};
+              let bSiaLatLng = {lat: this.Pop[3].lat, lng: this.Pop[3].lng};
+              let bChiLatLng = {lat: this.Pop[4].lat, lng: this.Pop[4].lng};
+              let bPhlLatLng = {lat: this.Pop[5].lat, lng: this.Pop[5].lng};
+              let bNanLatLng = {lat: this.Pop[6].lat, lng: this.Pop[6].lng};
+              let bAsoLatLng = {lat: this.Pop[7].lat, lng: this.Pop[7].lng};
+              let mRamLatLng = {lat: this.Pop[8].lat, lng: this.Pop[8].lng};
+              let mPheLatLng = {lat: this.Pop[9].lat, lng: this.Pop[9].lng};
+              let mSukLatLng = {lat: this.Pop[10].lat, lng: this.Pop[10].lng};
+              let aPhaLatLng = {lat: this.Pop[11].lat, lng: this.Pop[11].lng};
+              let aRatLatLng = {lat: this.Pop[12].lat, lng: this.Pop[12].lng};
+              let aMukLatLng = {lat: this.Pop[13].lat, lng: this.Pop[13].lng};
 
 
-          // test compare Locate
-          let CVicLat = latitude - this.Pop[0].lat;
-          let CVicLng = longitude - this.Pop[0].lng;
-          let CPhaLat = latitude - this.Pop[1].lat;
-          let CPhaLng = longitude - this.Pop[1].lng;
-          let CRatLat = latitude - this.Pop[2].lat;
-          let CRatLng = longitude - this.Pop[2].lng;
-          let CSiaLat = latitude - this.Pop[3].lat;
-          let CSiaLng = longitude - this.Pop[3].lng;
-          let CChiLat = latitude - this.Pop[4].lat;
-          let CChiLng = longitude - this.Pop[4].lng;
-          let CPhlLat = latitude - this.Pop[5].lat;
-          let CPhlLng = longitude - this.Pop[5].lng;
-          let CNanLat = latitude - this.Pop[6].lat;
-          let CNanLng = longitude - this.Pop[6].lng;
-          let CAsoLat = latitude - this.Pop[7].lat;
-          let CAsoLng = longitude - this.Pop[7].lng;
-          let CRamLat = latitude - this.Pop[8].lat;
-          let CRamLng = longitude - this.Pop[8].lng;
-          let CPheLat = latitude - this.Pop[9].lat;
-          let CPheLng = longitude - this.Pop[9].lng;
-          let CSukLat = latitude - this.Pop[10].lat;
-          let CSukLng = longitude - this.Pop[10].lng;
-          let CaPhaLat = latitude - this.Pop[11].lat;
-          let CaPhaLng = longitude - this.Pop[11].lng;
-          let CaRatLat = latitude - this.Pop[12].lat;
-          let CaRatLng = longitude - this.Pop[12].lng;
-          let CMukLat = latitude - this.Pop[13].lat;
-          let CMukLng = longitude - this.Pop[13].lng;
-  
-  
-          if (CVicLat<0) {
-            CVicLat = CVicLat*-1;
-          } 
-          console.log(CVicLat);
-          if (CVicLng<0) {
-            CVicLng = CVicLng*-1;
-          }
-          console.log(CVicLng);
-          if (CPhaLat<0) {
-            CPhaLat = CPhaLat*-1;
-          } 
-          console.log(CPhaLat);
-          if (CPhaLng<0) {
-            CPhaLng = CPhaLng*-1;
-          } 
-          console.log(CPhaLng);
-          if (CRatLat<0) {
-            CRatLat = CRatLat*-1;
-          }
-          console.log(CRatLat);
-          if (CRatLng<0) {
-            CRatLng = CRatLng*-1;
-          }
-          console.log(CRatLng);
-          if (CSiaLat<0) {
-            CSiaLat = CSiaLat*-1;
-          }
-          console.log(CSiaLat);
-          if (CSiaLng<0) {
-            CSiaLng = CSiaLng*-1;
-          }
-          console.log(CSiaLng);
-          if (CChiLat<0) {
-            CChiLat = CChiLat*-1;
-          }
-          console.log(CChiLat);
-          if (CChiLng<0) {
-            CChiLng = CChiLng*-1;
-          }
-          console.log(CChiLng);
-          if (CPhlLat<0) {
-            CPhlLat = CPhlLat*-1;
-          }
-          console.log(CPhlLat);
-          if (CPhlLng<0) {
-            CPhlLng = CPhlLng*-1;
-          }
-          console.log(CPhlLng);
-          if (CNanLat<0) {
-            CNanLat = CNanLat*-1;
-          }
-          console.log(CNanLat);
-          if (CNanLng<0) {
-            CNanLng = CNanLng*-1;
-          }
-          console.log(CNanLng);
-          if (CAsoLat<0) {
-            CAsoLat = CAsoLat*-1;
-          }
-          console.log(CAsoLat);
-          if (CAsoLng<0) {
-            CAsoLng = CAsoLng*-1;
-          }
-          console.log(CAsoLng);
-          if (CRamLat<0) {
-            CRamLat = CRamLat*-1;
-          }
-          console.log(CRamLat);
-          if (CRamLng<0) {
-            CRamLng = CRamLng*-1;
-          }
-          console.log(CRamLng);
-          if (CPheLat<0) {
-            CPheLat = CPheLat*-1;
-          }
-          console.log(CPheLat);
-          if (CPheLng<0) {
-            CPheLng = CPheLng*-1;
-          }
-          console.log(CPheLng);
-          if (CSukLat<0) {
-            CSukLat = CSukLat*-1;
-          }
-          console.log(CSukLat);
-          if (CSukLng<0) {
-            CSukLng = CSukLng*-1;
-          }
-          console.log(CSukLng);
-          if (CaPhaLat<0) {
-            CaPhaLat = CaPhaLat*-1;
-          }
-          console.log(CaPhaLat);
-          if (CaPhaLng<0) {
-            CaPhaLng = CaPhaLng*-1;
-          }
-          console.log(CaPhaLng);
-          if (CaRatLat<0) {
-            CaRatLat = CaRatLat*-1;
-          }
-          console.log(CaRatLat);
-          if (CaRatLng<0) {
-            CaRatLng = CaRatLng*-1;
-          }
-          console.log(CaRatLng);
-          if (CMukLat<0) {
-            CMukLat = CMukLat*-1;
-          }
-          console.log(CMukLat);
-          if (CMukLng<0) {
-            CMukLng = CMukLng*-1;
-          }
-          console.log(CMukLng);
-  
-  
-          if ((CPhaLat>CVicLat)&&(CPhaLng>CVicLng)) {
-            this.end = bVicLatLng;
-            console.log('Vic Closer') ; 
-          } else if ((CRatLat>CPhaLat)&&(CRatLng>CPhaLat)) {
-            this.end = bPhaLatLng;
-            console.log('Pha Closer') ;         
-          } else if ((CSiaLat>CRatLat)&&(CSiaLng>CRatLng)) {
-            this.end = bRatLatLng;
-          } else if((CChiLat>CSiaLat)&&(CChiLng>CSiaLng)){
-            this.end = bSiaLatLng;
-          } else if((CPhlLat>CChiLat)&&(CPhlLng>CChiLng)){
-            this.end = bChiLatLng;
-          } else if((CNanLat>CPhlLat)&&(CNanLng>CPhlLng)){
-            this.end = bPhlLatLng;
-          } else if((CAsoLat>CNanLat)&&(CAsoLng>CNanLng)){
-            this.end = bNanLatLng;
-          } else if((CRamLat>CAsoLat)&&(CRamLng>CAsoLng)){
-            this.end = bAsoLatLng;
-          } else if((CPheLat>CRamLat)&&(CPheLng>CRamLng)){
-            this.end = mRamLatLng;
-          } else if((CSukLat>CPheLat)&&(CSukLng>CPheLng)){
-            this.end = mPheLatLng;
-          } else if((CaPhaLat>CSukLat)&&(CaPhaLng>CSukLng)){
-            this.end = mSukLatLng;
-          } else if((CaRatLat>CaPhaLat)&&(CaRatLng>CaPhaLng)){
-            this.end = aPhaLatLng;
-          } else if((CMukLat>CaRatLat)&&(CMukLng>CaRatLng)){
-            this.end = aRatLatLng;
-          } else{
-            this.end = aMukLatLng;
-          }
+              // test compare Locate
 
-  
+              this.calculateDistance(this.start.lat,bVicLatLng.lat,this.start.lng,bVicLatLng.lng);
+              console.log('Vic');
+              this.calculateDistance(this.start.lat,bPhaLatLng.lat,this.start.lng,bPhaLatLng.lng);
+              console.log('Pha');
+              this.calculateDistance(this.start.lat,bRatLatLng.lat,this.start.lng,bRatLatLng.lng);
+              console.log('bRat');
+              this.calculateDistance(this.start.lat,bSiaLatLng.lat,this.start.lng,bSiaLatLng.lng);
+              console.log('Sia');
+              this.calculateDistance(this.start.lat,bChiLatLng.lat,this.start.lng,bChiLatLng.lng);
+              console.log('Chi');
+              this.calculateDistance(this.start.lat,bPhlLatLng.lat,this.start.lng,bPhlLatLng.lng);
+              console.log('Phl');
+              this.calculateDistance(this.start.lat,bNanLatLng.lat,this.start.lng,bNanLatLng.lng);
+              console.log('Nan');
+              this.calculateDistance(this.start.lat,bAsoLatLng.lat,this.start.lng,bAsoLatLng.lng);
+              console.log('Aso');
+              this.calculateDistance(this.start.lat,mRamLatLng.lat,this.start.lng,mRamLatLng.lng);
+              console.log('mRam');
+              this.calculateDistance(this.start.lat,mPheLatLng.lat,this.start.lng,mPheLatLng.lng);
+              console.log('mPhe');
+              this.calculateDistance(this.start.lat,mSukLatLng.lat,this.start.lng,mSukLatLng.lng);
+              console.log('Suk');
+              this.calculateDistance(this.start.lat,aPhaLatLng.lat,this.start.lng,aPhaLatLng.lng);
+              console.log('aPha');
+              this.calculateDistance(this.start.lat,aRatLatLng.lat,this.start.lng,aRatLatLng.lng);
+              console.log('aRat');
+              this.calculateDistance(this.start.lat,aMukLatLng.lat,this.start.lng,aMukLatLng.lng);
+              console.log('aMuk');
+//เหลือแก้ latlng ในเบสเอาตัวแปรมาเก็บก็เสร็จ
+              // let CVicLat = latitude - this.Pop[0].lat;
+              // let CVicLng = longitude - this.Pop[0].lng;
+              // let CPhaLat = latitude - this.Pop[1].lat;
+              // let CPhaLng = longitude - this.Pop[1].lng;
+              // let CRatLat = latitude - this.Pop[2].lat;
+              // let CRatLng = longitude - this.Pop[2].lng;
+              // let CSiaLat = latitude - this.Pop[3].lat;
+              // let CSiaLng = longitude - this.Pop[3].lng;
+              // let CChiLat = latitude - this.Pop[4].lat;
+              // let CChiLng = longitude - this.Pop[4].lng;
+              // let CPhlLat = latitude - this.Pop[5].lat;
+              // let CPhlLng = longitude - this.Pop[5].lng;
+              // let CNanLat = latitude - this.Pop[6].lat;
+              // let CNanLng = longitude - this.Pop[6].lng;
+              // let CAsoLat = latitude - this.Pop[7].lat;
+              // let CAsoLng = longitude - this.Pop[7].lng;
+              // let CRamLat = latitude - this.Pop[8].lat;
+              // let CRamLng = longitude - this.Pop[8].lng;
+              // let CPheLat = latitude - this.Pop[9].lat;
+              // let CPheLng = longitude - this.Pop[9].lng;
+              // let CSukLat = latitude - this.Pop[10].lat;
+              // let CSukLng = longitude - this.Pop[10].lng;
+              // let CaPhaLat = latitude - this.Pop[11].lat;
+              // let CaPhaLng = longitude - this.Pop[11].lng;
+              // let CaRatLat = latitude - this.Pop[12].lat;
+              // let CaRatLng = longitude - this.Pop[12].lng;
+              // let CMukLat = latitude - this.Pop[13].lat;
+              // let CMukLng = longitude - this.Pop[13].lng;
+        
+        
+              //   if (CVicLat<0) {
+              //   CVicLat = CVicLat*-1;
+              // } 
+              // // console.log(CVicLat);
+              // if (CVicLng<0) {
+              //   CVicLng = CVicLng*-1;
+              // }
+              // // console.log(CVicLng);
+              // if (CPhaLat<0) {
+              //   CPhaLat = CPhaLat*-1;
+              // } 
+              // // console.log(CPhaLat);
+              // if (CPhaLng<0) {
+              //   CPhaLng = CPhaLng*-1;
+              // } 
+              // // console.log(CPhaLng);
+              // if (CRatLat<0) {
+              //   CRatLat = CRatLat*-1;
+              // }
+              // // console.log(CRatLat);
+              // if (CRatLng<0) {
+              //   CRatLng = CRatLng*-1;
+              // }
+              // // console.log(CRatLng);
+              // if (CSiaLat<0) {
+              //   CSiaLat = CSiaLat*-1;
+              // }
+              // // console.log(CSiaLat);
+              // if (CSiaLng<0) {
+              //   CSiaLng = CSiaLng*-1;
+              // }
+              // // console.log(CSiaLng);
+              // if (CChiLat<0) {
+              //   CChiLat = CChiLat*-1;
+              // }
+              // // console.log(CChiLat);
+              // if (CChiLng<0) {
+              //   CChiLng = CChiLng*-1;
+              // }
+              // // console.log(CChiLng);
+              // if (CPhlLat<0) {
+              //   CPhlLat = CPhlLat*-1;
+              // }
+              // // console.log(CPhlLat);
+              // if (CPhlLng<0) {
+              //   CPhlLng = CPhlLng*-1;
+              // }
+              // // console.log(CPhlLng);
+              // if (CNanLat<0) {
+              //   CNanLat = CNanLat*-1;
+              // }
+              // // console.log(CNanLat);
+              // if (CNanLng<0) {
+              //   CNanLng = CNanLng*-1;
+              // }
+              // // console.log(CNanLng);
+              // if (CAsoLat<0) {
+              //   CAsoLat = CAsoLat*-1;
+              // }
+              // // console.log(CAsoLat);
+              // if (CAsoLng<0) {
+              //   CAsoLng = CAsoLng*-1;
+              // }
+              // // console.log(CAsoLng);
+              // if (CRamLat<0) {
+              //   CRamLat = CRamLat*-1;
+              // }
+              // // console.log(CRamLat);
+              // if (CRamLng<0) {
+              //   CRamLng = CRamLng*-1;
+              // }
+              // // console.log(CRamLng);
+              // if (CPheLat<0) {
+              //   CPheLat = CPheLat*-1;
+              // }
+              // // console.log(CPheLat);
+              // if (CPheLng<0) {
+              //   CPheLng = CPheLng*-1;
+              // }
+              // // console.log(CPheLng);
+              // if (CSukLat<0) {
+              //   CSukLat = CSukLat*-1;
+              // }
+              // // console.log(CSukLat);
+              // if (CSukLng<0) {
+              //   CSukLng = CSukLng*-1;
+              // }
+              // // console.log(CSukLng);
+              // if (CaPhaLat<0) {
+              //   CaPhaLat = CaPhaLat*-1;
+              // }
+              // // console.log(CaPhaLat);
+              // if (CaPhaLng<0) {
+              //   CaPhaLng = CaPhaLng*-1;
+              // }
+              // // console.log(CaPhaLng);
+              // if (CaRatLat<0) {
+              //   CaRatLat = CaRatLat*-1;
+              // }
+              // // console.log(CaRatLat);
+              // if (CaRatLng<0) {
+              //   CaRatLng = CaRatLng*-1;
+              // }
+              // // console.log(CaRatLng);
+              // if (CMukLat<0) {
+              //   CMukLat = CMukLat*-1;
+              // }
+              // // console.log(CMukLat);
+              // if (CMukLng<0) {
+              //   CMukLng = CMukLng*-1;
+              // }
+              // // console.log(CMukLng);
+      
+      
+              // if ((CPhaLat>CVicLat)&&(CPhaLng>CVicLng)) {
+              //   this.end = bVicLatLng;
+              //   console.log('Vic Closer') ; 
+              // } else if ((CRatLat>CPhaLat)&&(CRatLng>CPhaLat)) {
+              //   this.end = bPhaLatLng;
+              //   console.log('Pha Closer') ;         
+              // } else if ((CSiaLat>CRatLat)&&(CSiaLng>CRatLng)) {
+              //   this.end = bRatLatLng;
+              // } else if((CChiLat>CSiaLat)&&(CChiLng>CSiaLng)){
+              //   this.end = bSiaLatLng;
+              // } else if((CPhlLat>CChiLat)&&(CPhlLng>CChiLng)){
+              //   this.end = bChiLatLng;
+              // } else if((CNanLat>CPhlLat)&&(CNanLng>CPhlLng)){
+              //   this.end = bPhlLatLng;
+              // } else if((CAsoLat>CNanLat)&&(CAsoLng>CNanLng)){
+              //   this.end = bNanLatLng;
+              // } else if((CRamLat>CAsoLat)&&(CRamLng>CAsoLng)){
+              //   this.end = bAsoLatLng;
+              // } else if((CPheLat>CRamLat)&&(CPheLng>CRamLng)){
+              //   this.end = mRamLatLng;
+              // } else if((CSukLat>CPheLat)&&(CSukLng>CPheLng)){
+              //   this.end = mPheLatLng;
+              // } else if((CaPhaLat>CSukLat)&&(CaPhaLng>CSukLng)){
+              //   this.end = mSukLatLng;
+              // } else if((CaRatLat>CaPhaLat)&&(CaRatLng>CaPhaLng)){
+              //   this.end = aPhaLatLng;
+              // } else if((CMukLat>CaRatLat)&&(CMukLng>CaRatLng)){
+              //   this.end = aRatLatLng;
+              // } else{
+              //   this.end = aMukLatLng;
+              // }
+
+              
+
+
           // 
   
           // console.log(bVicLatLng);
@@ -425,12 +469,17 @@ export class MapPage {
           //   map: this.map,
           //   title: 'Mukason' 
           // });
+
+
+          
+
           mapEle.classList.add('show-map');
         });
       })
       // });
       
 }
+
 
   updateSearchResults(){
     if (this.autocomplete.input == '') {
